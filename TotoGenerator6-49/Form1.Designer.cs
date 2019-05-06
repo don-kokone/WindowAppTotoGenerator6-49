38fd7effefe91cf1087eeac0cb221abd9ec14ee2
don-kokone <don_kokone@start.bg>
2019-05-06 22:49:10 +0300

    Commit project

---
+++ TotoGenerator6-49/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace TotoGenerator6_49
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.slot1 = new System.Windows.Forms.Panel();
+            this.slot2 = new System.Windows.Forms.Panel();
+            this.slot3 = new System.Windows.Forms.Panel();
+            this.slot4 = new System.Windows.Forms.Panel();
+            this.slot5 = new System.Windows.Forms.Panel();
+            this.slot6 = new System.Windows.Forms.Panel();
+            this.num1 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.Label();
+            this.num3 = new System.Windows.Forms.Label();
+            this.num4 = new System.Windows.Forms.Label();
+            this.num5 = new System.Windows.Forms.Label();
+            this.num6 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.slot1.SuspendLayout();
+            this.slot2.SuspendLayout();
+            this.slot3.SuspendLayout();
+            this.slot4.SuspendLayout();
+            this.slot5.SuspendLayout();
+            this.slot6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DimGray;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(511, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(92, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 88);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // slot1
+            // 
+            this.slot1.BackColor = System.Drawing.Color.White;
+            this.slot1.Controls.Add(this.num1);
+            this.slot1.Location = new System.Drawing.Point(9, 105);
+            this.slot1.Name = "slot1";
+            this.slot1.Size = new System.Drawing.Size(77, 76);
+            this.slot1.TabIndex = 2;
+            // 
+            // slot2
+            // 
+            this.slot2.BackColor = System.Drawing.Color.White;
+            this.slot2.Controls.Add(this.num2);
+            this.slot2.Location = new System.Drawing.Point(92, 105);
+            this.slot2.Name = "slot2";
+            this.slot2.Size = new System.Drawing.Size(77, 76);
+            this.slot2.TabIndex = 3;
+            // 
+            // slot3
+            // 
+            this.slot3.BackColor = System.Drawing.Color.White;
+            this.slot3.Controls.Add(this.num3);
+            this.slot3.Location = new System.Drawing.Point(175, 105);
+            this.slot3.Name = "slot3";
+            this.slot3.Size = new System.Drawing.Size(77, 76);
+            this.slot3.TabIndex = 4;
+            // 
+            // slot4
+            // 
+            this.slot4.BackColor = System.Drawing.Color.White;
+            this.slot4.Controls.Add(this.num4);
+            this.slot4.Location = new System.Drawing.Point(258, 105);
+            this.slot4.Name = "slot4";
+            this.slot4.Size = new System.Drawing.Size(77, 76);
+            this.slot4.TabIndex = 5;
+            // 
+            // slot5
+            // 
+            this.slot5.BackColor = System.Drawing.Color.White;
+            this.slot5.Controls.Add(this.num5);
+            this.slot5.Location = new System.Drawing.Point(341, 105);
+            this.slot5.Name = "slot5";
+            this.slot5.Size = new System.Drawing.Size(77, 76);
+            this.slot5.TabIndex = 6;
+            // 
+            // slot6
+            // 
+            this.slot6.BackColor = System.Drawing.Color.White;
+            this.slot6.Controls.Add(this.num6);
+            this.slot6.Location = new System.Drawing.Point(424, 105);
+            this.slot6.Name = "slot6";
+            this.slot6.Size = new System.Drawing.Size(77, 76);
+            this.slot6.TabIndex = 7;
+            // 
+            // num1
+            // 
+            this.num1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.num1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.num1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(0, 0);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(77, 76);
+            this.num1.TabIndex = 0;
+            this.num1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // num2
+            // 
+            this.num2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.num2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.num2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(0, 0);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(77, 76);
+            this.num2.TabIndex = 0;
+            this.num2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // num3
+            // 
+            this.num3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.num3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.num3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num3.Location = new System.Drawing.Point(0, 0);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(77, 76);
+            this.num3.TabIndex = 0;
+            this.num3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // num4
+            // 
+            this.num4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.num4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.num4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num4.Location = new System.Drawing.Point(0, 0);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(77, 76);
+            this.num4.TabIndex = 0;
+            this.num4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // num5
+            // 
+            this.num5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.num5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.num5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num5.Location = new System.Drawing.Point(0, 0);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(77, 76);
+            this.num5.TabIndex = 0;
+            this.num5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // num6
+            // 
+            this.num6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.num6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.num6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num6.Location = new System.Drawing.Point(0, 0);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(77, 76);
+            this.num6.TabIndex = 0;
+            this.num6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(511, 492);
+            this.Controls.Add(this.slot6);
+            this.Controls.Add(this.slot5);
+            this.Controls.Add(this.slot4);
+            this.Controls.Add(this.slot3);
+            this.Controls.Add(this.slot2);
+            this.Controls.Add(this.slot1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Toto 6/49 generator";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.slot1.ResumeLayout(false);
+            this.slot2.ResumeLayout(false);
+            this.slot3.ResumeLayout(false);
+            this.slot4.ResumeLayout(false);
+            this.slot5.ResumeLayout(false);
+            this.slot6.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel slot1;
+        private System.Windows.Forms.Panel slot2;
+        private System.Windows.Forms.Panel slot3;
+        private System.Windows.Forms.Panel slot4;
+        private System.Windows.Forms.Panel slot5;
+        private System.Windows.Forms.Panel slot6;
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Label num2;
+        private System.Windows.Forms.Label num3;
+        private System.Windows.Forms.Label num4;
+        private System.Windows.Forms.Label num5;
+        private System.Windows.Forms.Label num6;
+    }
+}
+
---
+++ TotoGenerator6-49/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TotoGenerator6_49
+{
+    public partial class Form1 : Form
+    {
+
+        Random numGen = new Random();
+        int val = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanks for using it!\n\nby Kaloyan Zahariev! \n \n v. 1.0", "About this program");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generateNumbers();
+        }
+
+        private void generateNumbers()
+        {
+            val = numGen.Next(1, 50);
+            num1.Text = val.ToString();
+
+            val = numGen.Next(1, 50);
+            while (val.ToString().Equals(num1.Text))
+            {
+                val = numGen.Next(1, 50); 
+            }
+            num2.Text = val.ToString();
+
+            while (val.ToString().Equals(num1.Text) || val.ToString().Equals(num2.Text))
+            {
+                val = numGen.Next(1, 50);
+            }
+            num3.Text = val.ToString();
+
+            while (val.ToString().Equals(num1.Text) || val.ToString().Equals(num2.Text) || val.ToString().Equals(num3.Text))
+            {
+                val = numGen.Next(1, 50);
+            }
+            num4.Text = val.ToString();
+
+
+            while (val.ToString().Equals(num1.Text) || val.ToString().Equals(num2.Text) || val.ToString().Equals(num3.Text) || val.ToString().Equals(num4.Text))
+            {
+                val = numGen.Next(1, 50);
+            }
+            num5.Text = val.ToString();
+
+            while (val.ToString().Equals(num1.Text) || val.ToString().Equals(num2.Text) || val.ToString().Equals(num3.Text) || val.ToString().Equals(num4.Text) || val.ToString().Equals(num5.Text))
+            {
+                val = numGen.Next(1, 50);
+            }
+            num6.Text = val.ToString();
+        }
+    }
+}
